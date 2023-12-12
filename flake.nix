da9cd3a29e9c9dfde3b41919440c1d2cf2b1f449
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2023-12-12 15:14:02 +0530

    add iac flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Basic dev tools to start IaC using Hashicorp tools";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          packer
+          terraform
+          terragrunt
+        ];
+      };
+    };
+}
